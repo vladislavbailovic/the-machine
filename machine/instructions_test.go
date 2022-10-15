0641444dbcc568420adb0c823da11918d0f09681
Vladislav Bailovic <vladislavbailovic@gmail.com>
2022-10-15 11:36:04 +0200

    Simplify loop test

--- machine/instructions_test.go
+++ machine/instructions_test.go
@@ -31,17 +31,12 @@ func Test_Execute_Program(t *testing.T) {
 
 func Test_Execute_Loop(t *testing.T) {
 	cpu := NewCpu()
-	b1 := make([]byte, 2)
-	binary.LittleEndian.PutUint16(b1, uint16(1))
-	b2 := make([]byte, 2)
-	binary.LittleEndian.PutUint16(b2, uint16(3))
 	cpu.LoadProgram(0, []byte{
-		byte(instruction.MOV_LIT_AC), b1[0], b1[1],
-		byte(instruction.NOP),
-		byte(instruction.MOV_LIT_R1), b1[0], b1[1],
+		byte(instruction.MOV_LIT_AC), 0x01, 0x00,
+		byte(instruction.MOV_LIT_R1), 0x01, 0x00,
 		byte(instruction.ADD_REG_REG), byte(register.Ac), byte(register.R1),
-		byte(instruction.JNE), b2[0], b2[1], 0x04, 0x00,
-		byte(instruction.MOV_LIT_R2), 0xac, 0x0b0,
+		byte(instruction.JNE), 0x03, 0x00, 0x06, 0x00,
+		byte(instruction.MOV_LIT_R2), 0xac, 0xab,
 	})
 
 	step := 0

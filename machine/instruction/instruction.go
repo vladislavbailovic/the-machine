6619b1767df01fbd8f6747fbcd1fc180bff0905e
vladislav <ve@incsub.com>
2022-10-22 09:29:00 +0200

    Simplify instructions

--- machine/instruction/instruction.go
+++ machine/instruction/instruction.go
@@ -9,7 +9,6 @@ import (
 
 type Instruction struct {
 	Description string
-	Parameters  []Parameter
 	Raw         uint16
 	Executor    Executor
 }
@@ -28,56 +27,7 @@ func (x Instruction) Execute(cpu *cpu.Cpu, memory *memory.Memory) error {
 
 // TODO: move param parsing to machine::decode
 func (x Instruction) getParams(cpu *cpu.Cpu, mem *memory.Memory) ([]byte, error) {
-	/*
-			works for one arg
-			params := []byte{
-				byte(x.Raw & 0b0000_0000_1111_1111),
-				byte((x.Raw >> 8)),
-			}
-		fmt.Printf("\nraw:%016b\none:%016b\ntwo:%016b\n----------\n", x.Raw, params[0], params[1])
-	*/
-
 	b := make([]byte, 2)
 	binary.LittleEndian.PutUint16(b, x.Raw)
-
-	// params := []byte{b1, b2}
-	// return params, nil
 	return b, nil
-
-	// length := 0
-	// for _, p := range x.Parameters {
-	// 	length += p.GetBytesLength()
-	// }
-	// params := []byte{}
-
-	// pos := cpu.GetRegister(register.Ip)
-	// for idx, p := range x.Parameters {
-	// 	switch p {
-	// 	case ParamRegister:
-	// 		val, err := mem.GetByte(memory.Address(pos))
-	// 		if err != nil {
-	// 			return params, fmt.Errorf("%s: error getting param %d: %v", x.Description, idx, err)
-	// 		}
-	// 		pos++
-	// 		params = append(params, val)
-	// 	case ParamLiteral, ParamAddress:
-	// 		hi, err := mem.GetByte(memory.Address(pos))
-	// 		if err != nil {
-	// 			return params, fmt.Errorf("%s: error getting param %d: %v", x.Description, idx, err)
-	// 		}
-	// 		params = append(params, hi)
-	// 		pos++
-	// 		lo, err := mem.GetByte(memory.Address(pos))
-	// 		if err != nil {
-	// 			return params, fmt.Errorf("%s: error getting param %d: %v", x.Description, idx, err)
-	// 		}
-	// 		pos++
-	// 		params = append(params, lo)
-	// 	default:
-	// 		return params, fmt.Errorf("unexpected parameter: %d", p)
-	// 	}
-	// }
-	// cpu.SetRegister(register.Ip, pos)
-
-	// return params, nil
 }
--- main.go
+++ main.go
@@ -12,7 +12,6 @@ import (
 func main() {
 	vm := machine.NewMachine(255)
 	vm.LoadProgram(0, []byte{
-		instruction.MOV_LIT_AC.AsByte(), 0x02, 0x00,
 		instruction.MUL_REG_LIT.AsByte(), register.Ac.AsByte(), 0x02, 0x00,
 		instruction.JLT.AsByte(), 0x07, 0x00, 0x03, 0x00,
 		instruction.MOD_REG_LIT.AsByte(), register.Ac.AsByte(), 0x03, 0x00,
